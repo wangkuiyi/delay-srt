d99336be3717c6c09babcc74b20bdacb04fff567
Yi Wang <wangyi@farefore.com>
2012-07-29 14:41:36 +0800

    First commit

---
+++ delay-srt-test.rkt
@@ -0,0 +1,54 @@
+#lang racket/base
+
+(require rackunit
+         "sa.rkt")
+
+(check-equal? (parse-timestamp "12:34:56,999")
+              '(999 56 34 12))
+
+(check-equal? (format-timestamp (parse-timestamp "12:34:56,999"))
+              "12:34:56,999")
+
+(check-equal? (add-timestamp (parse-timestamp "59:59:59,999")
+                             (parse-timestamp "10000:1:1,1"))
+              (parse-timestamp "10060:1:1,0"))
+
+(check-equal? (sub-timestamp (parse-timestamp "10000:1:1,1")
+                             (parse-timestamp "99:59:59,999"))
+              (parse-timestamp "9900:1:1,2"))
+
+(check-equal? (parse-time-interval "00:00:32,620 --> 00:00:35,490")
+              (list (parse-timestamp "00:00:32,620")
+                    (parse-timestamp "00:00:35,490")))
+
+(check-equal? (parse-time-interval "whatever") #f)
+
+(check-equal? (format-time-interval
+               (parse-time-interval "00:00:32,620 --> 00:00:35,490"))
+              "0:0:32,620 --> 0:0:35,490")
+
+(check-equal? (let ((out (open-output-string)))
+                (change-delay-of-srt-file
+                 (open-input-string
+                  (string-append "1\n"
+                                 "00:00:00,000 --> 00:00:03,000\n"
+                                 "■\n"
+                                 "\n"
+                                 "2\n"
+                                 "00:00:04,640 --> 00:00:06,840\n"
+                                 "本字幕由 YYeTs人人影视 原创翻译制作\n"
+                                 "仅供学习 禁止用于任何商业盈利行为\n"
+                                 " 更多影视更新 请登陆 www.YYeTs.net\n"))
+                 out
+                 (make-timestamp-change 'increase (parse-timestamp "1:1:1,999")))
+                (get-output-string out))
+
+              (string-append "1\n"
+                             "1:1:1,999 --> 1:1:4,999\n"
+                             "■\n"
+                             "\n"
+                             "2\n"
+                             "1:1:6,639 --> 1:1:8,839\n"
+                             "本字幕由 YYeTs人人影视 原创翻译制作\n"
+                             "仅供学习 禁止用于任何商业盈利行为\n"
+                             " 更多影视更新 请登陆 www.YYeTs.net\n"))
---
+++ delay-srt.rkt
@@ -0,0 +1,121 @@
+#lang racket
+
+(require racket/cmdline)
+
+(provide parse-timestamp
+         format-timestamp
+         add-timestamp
+         sub-timestamp
+         parse-time-interval
+         format-time-interval
+         make-timestamp-change
+         change-delay-of-srt-file)
+
+
+;; Parse a string in the format of "hh:mm:ss:mil" into a timestamp.
+;; A timestamp value is a list: '(milliseconds, seconds, minutes, hours).
+(define (parse-timestamp string)
+  (let* ((pattern "(\\d+):(\\d+):(\\d+),(\\d+)")
+         (parsed (regexp-match (pregexp pattern) string)))
+    (cond ((false? parsed) (raise "Failed parsing the delay time."))
+          (else (reverse (map string->number (cdr parsed)))))))
+
+
+;; Write a timestamp into SRT format string.
+(define (format-timestamp s)
+  (format "~a:~a:~a,~a"
+          (list-ref s 3)
+          (list-ref s 2)
+          (list-ref s 1)
+          (list-ref s 0)))
+
+;; Add two timestamps.
+(define (add-timestamp s1 s2)
+  (let ((carry '(1000 60 60))) ; hours do not carry
+    (let add-rec
+        ((l1 s1) (l2 s2) (carry carry) (given 0) (result '()))
+      (if (= (length l1) 1) ; we are processing hours
+          (reverse (cons (+ (car l1) (car l2) given) result))
+          (let ((cur-value (+ (car l1) (car l2) given)))
+            (add-rec (cdr l1) (cdr l2) (cdr carry)
+                     (quotient cur-value (car carry))
+                     (cons (remainder cur-value (car carry)) result)))))))
+
+;; Substract two timestamps.
+(define (sub-timestamp s1 s2)
+  (let ((carry '(1000 60 60)))
+    (let sub-rec
+        ((l1 s1) (l2 s2) (carry carry) (result '()))
+      (if (= (length l1) 1)
+          (reverse (cons (- (car l1) (car l2)) result))
+          (let ((cur-value (- (car l1) (car l2))))
+            (if (>= cur-value 0)
+                (sub-rec (cdr l1) (cdr l2) (cdr carry) (cons cur-value result))
+                (sub-rec (cons (- (cadr l1) 1) (cddr l1)) (cdr l2) (cdr carry)
+                         (cons (+ cur-value (car carry)) result))))))))
+
+;; Parse time interval as two timestamps.
+;; Returns either a list of two timestamps or #f.
+(define (parse-time-interval line)
+  (let* ((pattern (string-append "^"
+                                 "(\\d+:\\d+:\\d+,\\d+)"
+                                 " +--> +"
+                                 "(\\d+:\\d+:\\d+,\\d+)"
+                                 "$"))
+         (matched (regexp-match (pregexp pattern) line)))
+    (cond ((false? matched) #f)
+          (else (list (parse-timestamp (list-ref matched 1))
+                      (parse-timestamp (list-ref matched 2)))))))
+
+
+;; Write a time interval into SRT format string.
+(define (format-time-interval interval)
+  (format "~a --> ~a"
+          (format-timestamp (car interval))
+          (format-timestamp (cadr interval))))
+
+
+;; Given a symbol of 'increase or 'decrease, returns a lambda which
+;; increase or decrease delay to its timestamp parameter.
+(define (make-timestamp-change change delay)
+  (cond ((symbol=? change 'increase)
+         (lambda (s) (add-timestamp s delay)))
+        ((symbol=? change 'decrease)
+         (lambda (s) (sub-timestamp s delay)))
+        (else (raise "Unknown change keyword."))))
+
+
+;; Parse an SRT file, change its time interval lines, outputs to port.
+;; The file MUST be UTF-8 encoded.
+(define (change-delay-of-srt-file in out change)
+  (for ((line (in-lines in)))
+    (let ((interval (parse-time-interval line)))
+      (fprintf out "~a\n"
+               (if (false? interval)
+                   line
+                   (format-time-interval
+                    (map change interval)))))))
+
+
+;; Define command line flags as parameters.
+(define make-delay (make-parameter
+                    (make-timestamp-change
+                     'increase (parse-timestamp "0:0:0,0"))))
+
+(define srt-file-to-delay
+  (command-line
+   #:program "compiler"
+   #:once-any
+   (("-i" "--increase-delay") inc
+    "Increase the delay to the given SRT file, with format hh:mm:ss,mill"
+    (make-delay (make-timestamp-change 'increase (parse-timestamp inc))))
+   (("-d" "--decrease-delay") dec
+    "Decrease the delay to the given SRT file"
+    (make-delay (make-timestamp-change 'decrease (parse-timestamp dec))))
+   #:args (filename)    ; expect one command-line argument: <filename>
+                        ; return the argument as a filename to compile
+   filename))
+
+(call-with-input-file srt-file-to-delay
+  (lambda (in)
+    (change-delay-of-srt-file in (current-output-port) (make-delay))))
